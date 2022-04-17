3f45348f8dd36514137dfe0d00d447eaf95199d8
BunnyNomics101 <89548046+BunnyNomics101@users.noreply.github.com>
2022-04-17 13:16:28 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/bg.svg', // This path is relative to the root of the site.
+        icon: 'src/images/bg2.svg', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
