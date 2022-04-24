792bf9561838c9aa91a609b59f6e6b5b409174d4
dalideco <dalissj1@gmail.com>
2022-04-24 01:28:43 +0100

    styling the add address menu

--- src/components/dashboard/AddButton.js
+++ src/components/dashboard/AddButton.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import PriceInfo from '../../constants/PriceInfo'
 
 export default function AddButton() {
 
     const [shown, setShown] = useState(false)
+
+    const [NFTAdress, setNFTAdress] = useState("");
 
     return (
         <>
@@ -19,7 +22,46 @@ export default function AddButton() {
                 id="add-menu"
                 className={shown ? "shown" : ""}
             >
+                <div>
+                    {/* prince info */}
+                    {Object.keys(PriceInfo).map((item, i) => {
+                        console.log(item, PriceInfo[item])
+                        return (
+                            <div className='row'>
+                                <div className="capital"> {item} </div>
+                                <div> {PriceInfo[item]} </div>
+                            </div>
+                        )
+                    })}
+                    <hr />
 
+                    {/* summary */}
+                    <div className='row'>
+                        <div> Summary </div>
+                        <div>
+                            1 NFT + 8.8 USDC
+                        </div>
+                    </div>
+                </div>
+
+                {/* input  */}
+                <div className='input'>
+                    <input
+                        type="text"
+                        placeholder='NFT Adress'
+                        value={NFTAdress}
+
+                        onChange={(e) => {
+                            setNFTAdress(e.target.value)
+                        }}
+                    />
+                    <button
+                        className='add'
+                        disabled={NFTAdress === ""}
+                    >
+                        <FontAwesomeIcon icon={faPlus} />
+                    </button>
+                </div>
             </div>
         </>
     )
---
+++ src/constants/PriceInfo.js
@@ -0,0 +1,8 @@
+const PriceInfo = {
+    NFT: 1,
+    worth: "80 USDC",
+    collaterize: "8 USDC",
+    "Service Fee": "0.8 USDC",
+}
+
+export default PriceInfo; 
