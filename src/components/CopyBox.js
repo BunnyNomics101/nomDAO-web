4ce8f86521f49d8ed3fb2227027590e5898f1c48
dalideco <dalissj1@gmail.com>
2022-04-24 02:11:05 +0100

    creating copybox component

--- src/components/CopyBox.js
+++ src/components/CopyBox.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
 
-export default function CopyBox({value}) {
+export default function CopyBox({ value }) {
+
+  const copy = () => {
+    navigator.clipboard.writeText(value)
+  }
+
   return (
-    <div>CopyBox</div>
+    <div className="copy-box">
+      {value}
+      <div
+        className="copy-button"
+        onClick={()=>{
+          copy()
+        }}
+      >
+        <FontAwesomeIcon
+          icon={faCopy}
+        />
+      </div>
+    </div>
   )
 }
